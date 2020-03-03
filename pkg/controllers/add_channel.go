22d50adbd5acdb1b4d9d95bdbc9e1fb9a722bb9c
ianzhang366 <ian.w.zhang@redhat.com>
2020-03-03 10:18:53 -0500

    change controller package name to controllers

--- pkg/controllers/add_channel.go
+++ pkg/controllers/add_channel.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package controller
+package controllers
 
 import (
 	chv1 "github.com/open-cluster-management/multicloud-operators-channel/pkg/controllers/channel"
