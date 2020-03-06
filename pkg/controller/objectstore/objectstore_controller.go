03e20dfb04edb3485668c189b56a37ca26fcc5da
ianzhang366 <ian.w.zhang@redhat.com>
2020-03-06 11:35:35 -0500

    fix rbac group and onboard sonarcloud

--- pkg/controller/objectstore/objectstore_controller.go
+++ pkg/controller/objectstore/objectstore_controller.go
@@ -109,8 +109,8 @@ type ReconcileDeployable struct {
 // and what is in the Deployable.Spec
 
 // a Deployment as an example Automatically generate RBAC rules to allow the Controller to read and write Deployments
-// +kubebuilder:rbac:groups=multicloud-apps.io,resources=deployables,verbs=get;list;watch;create;update;patch;delete
-// +kubebuilder:rbac:groups=multicloud-apps.io,resources=deployables/status,verbs=get;update;patch
+// +kubebuilder:rbac:groups=apps.open-cluster-management.io,resources=deployables,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=apps.open-cluster-management.io,resources=deployables/status,verbs=get;update;patch
 func (r *ReconcileDeployable) Reconcile(request reconcile.Request) (reconcile.Result, error) {
 	// Fetch the Deployable instance
 	instance := &dplv1.Deployable{}
