cc15e952a90372e2e2b1a2cb86ffefe30cdd9d15
Ian Zhang <izhang@redhat.com>
2020-06-16 11:12:17 -0400

    rollback the go lint version
    Signed-off-by: Ian Zhang <izhang@redhat.com>

--- build/run-code-lint.sh
+++ build/run-code-lint.sh
@@ -37,6 +37,6 @@ gem install mdl
 gem install awesome_bot
 
 # Install golangci-lint
-curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b "$(go env GOPATH)"/bin v1.27.0
+curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b "$(go env GOPATH)"/bin v1.20.0
 # Start lint task
 make lint
