0f6bbbadfdcab9125cd136a36c3beca9453d6dae
Ian Zhang <izhang@redhat.com>
2021-01-11 16:07:15 -0500

    adding new test cases for persisting CA

--- pkg/webhook/certificate_test.go
+++ pkg/webhook/certificate_test.go
@@ -15,10 +15,15 @@
 package webhook
 
 import (
+	"context"
 	"os"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	corev1 "k8s.io/api/core/v1"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
 	k8scertutil "k8s.io/client-go/util/cert"
 )
 
@@ -26,10 +31,12 @@ var _ = FDescribe("self-signed cert", func() {
 	var (
 		podNamespaceEnvVar = "POD_NAMESPACE"
 		webhookServiceName = "default"
+		webhookServiceNs   = "test-ns"
 		certDir            = "/tmp/tmp-cert"
+		certName           = "test-cert-name"
 	)
 
-	It("should create CA and store it in secret, private key pairs should be created as well", func() {
+	It("should create CA and store it in secret if secret doesn't exist, private key pairs should be created as well", func() {
 		os.Setenv(podNamespaceEnvVar, "test")
 		defer func() {
 			os.RemoveAll(certDir)
@@ -47,6 +54,39 @@ var _ = FDescribe("self-signed cert", func() {
 		Expect(err).Should(Succeed())
 		Expect(isReadCertAndKey).Should(BeTrue())
 
+		srtKey := types.NamespacedName{Name: webhookServiceName, Namespace: podNs}
+
+		srtIns := &corev1.Secret{}
+		Expect(k8sClient.Get(context.TODO(), srtKey, srtIns)).Should(Succeed())
+		defer func() {
+			Expect(k8sClient.Delete(context.TODO(), srtIns)).Should(Succeed())
+		}()
+
+		Expect(srtIns.Data["crt"]).ShouldNot(HaveLen(0))
+		Expect(srtIns.Data["key"]).ShouldNot(HaveLen(0))
 	})
 
+	It("should get self-signed CA cert from exist secret", func() {
+		cert := "my cert"
+		key := "my key"
+		srtIns := &corev1.Secret{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      webhookServiceName,
+				Namespace: webhookServiceNs,
+			},
+
+			Data: map[string][]byte{
+				"crt": []byte(cert),
+				"key": []byte(key),
+			},
+		}
+
+		Expect(k8sClient.Create(context.TODO(), srtIns)).Should(Succeed())
+
+		ca, err := getSelfSignedCACert(k8sClient, certName, types.NamespacedName{Name: srtIns.Name, Namespace: srtIns.Namespace})
+		Expect(err).Should(Succeed())
+
+		Expect(ca.Cert).Should(Equal(cert))
+		Expect(ca.Key).Should(Equal(key))
+	})
 })
