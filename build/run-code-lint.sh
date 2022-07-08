ba343324d58198712ccac378ec59b011ffc90225
Jonathan Marcantonio <jmarcant@redhat.com>
2022-07-08 15:17:17 -0400

    update go to 1.18 (#234)
    * update go to 1.18
    
    Signed-off-by: Jonathan Marcantonio <jmarcant@redhat.com>
    
    * fix linters
    
    Signed-off-by: Jonathan Marcantonio <jmarcant@redhat.com>
    
    * enable linters gocognit, nolintlint, typecheck
    
    Signed-off-by: Jonathan Marcantonio <jmarcant@redhat.com>

--- build/run-code-lint.sh
+++ build/run-code-lint.sh
@@ -37,7 +37,7 @@ gem install mdl
 gem install awesome_bot
 
 # Install golangci-lint
-curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b "$(go env GOPATH)"/bin v1.24.0
+curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b "$(go env GOPATH)"/bin v1.46.2
 
 # Start lint task
 make -f Makefile.prow lint-all
