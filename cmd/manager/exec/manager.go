8a25e0f13b7442cd0132fd3da9b567e3f26bdc81
Jonathan Marcantonio <jmarcant@redhat.com>
2023-05-01 15:03:22 -0400

    Enforce server side TLSv1.3
    Signed-off-by: Jonathan Marcantonio <jmarcant@redhat.com>

--- cmd/manager/exec/manager.go
+++ cmd/manager/exec/manager.go
@@ -146,7 +146,7 @@ func RunManager() {
 		LeaseDuration:           &options.LeaderElectionLeaseDuration,
 		RenewDeadline:           &options.LeaderElectionRenewDeadline,
 		RetryPeriod:             &options.LeaderElectionRetryPeriod,
-		WebhookServer:           &k8swebhook.Server{TLSMinVersion: "1.2"},
+		WebhookServer:           &k8swebhook.Server{TLSMinVersion: "1.3"},
 	})
 
 	if err != nil {
