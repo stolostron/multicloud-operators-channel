d8756f66f5a60cda4cc5fbe017bff48f437739a2
Jonathan Marcantonio <jmarcant@redhat.com>
2023-07-05 13:04:56 -0400

    Update build parameters
    Signed-off-by: Jonathan Marcantonio <jmarcant@redhat.com>

--- cmd/manager/exec/manager.go
+++ cmd/manager/exec/manager.go
@@ -146,7 +146,7 @@ func RunManager() {
 		LeaseDuration:           &options.LeaderElectionLeaseDuration,
 		RenewDeadline:           &options.LeaderElectionRenewDeadline,
 		RetryPeriod:             &options.LeaderElectionRetryPeriod,
-		WebhookServer:           &k8swebhook.Server{TLSMinVersion: "1.3"},
+		WebhookServer:           &k8swebhook.Server{TLSMinVersion: chv1.TLSMinVersionString},
 	})
 
 	if err != nil {
